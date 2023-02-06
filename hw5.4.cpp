4fa5e83ad66e13fd2b7ee68571eabce9d29d4d49
yanjiel <yanjieliu829@gmail.com>
2023-02-05 21:28:52 -0600

    final

--- hw5.4.cpp
+++ hw5.4.cpp
@@ -27,11 +27,6 @@ public:
 		return (*fun)(forward<T2>(arg));
 	}
 
-	/*void print() {
-		cout << "here" << endl;
-		cout << (*fun)(static_cast<T2>(3)) << endl;
-	}*/
-
 private:
 	T1 (*fun) (T2) = NULL;
 };
@@ -41,66 +36,9 @@ int Counter<T1, T2>::count = 0;
 
 
 
-//class CounterWrapper {
-//public:
-//	template<typename T1, typename T2>
-//	CounterWrapper(T1(*func)(T2)) {
-//		cout << typeid(func).name() << ", " << typeid(*func).name() << endl;
-//		Counter<T1, T2> *counter = new Counter<T1, T2>(func);
-//		cout << typeid(counter).name() << ", " << typeid(*counter).name() << endl;
-//	}
-//
-//	template<typename T1, typename T2>
-//	T1 operator()(T2&& arg) {
-//		return (Counter<T1, T2> *counter)(arg);
-//	}
-//
-//private:
-//	template<typename T1, typename T2>
-//	static Counter<T1, T2> counter;
-//
-//};
-//
-
-//
-//
-//class Counter2 {
-//public:
-//	Counter2() : count(0) {}
-//
-//	template<typename T1, typename T2>
-//	Counter2(T1(*func)(T2)) { 
-//		count = 0; 
-//		//T1 (*fun)(T2) = move(func); 
-//		fun = func;
-//	}
-//
-//	template<typename T3, typename T4>
-//	T3 operator()(T4&& arg) {
-//		count += 1;
-//		return fun<T3, T4>(forward<T4>(arg));
-//	}
-//
-//	/*template<typename t2>
-//	operator<T1>() (T2&& arg) {count += 1; }*/
-//
-//private:
-//	int count;
-//	template <typename T1, typename T2>
-//	static T1 (*fun) (T2);
-//
-//	template <typename T1, typename T2>
-//	static T1(*op) (T2);
-//
-//};
-//
-
-		
-
 int main() {
 
 	Counter cf(f);
-	//cf.print();
 	cf(1.3); cf(2.4);
 	cout << format("f was called {} times\n", cf.count);
 }
